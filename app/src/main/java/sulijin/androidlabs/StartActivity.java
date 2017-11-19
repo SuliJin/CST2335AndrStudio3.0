53c7d06655e6666f8dc6b52f41d4a61f41f2b141
Suli Jin <suli.jin@gmail.com>
2017-11-18 19:16:55 -0500

    Initial commit

---
+++ app/src/main/java/sulijin/androidlabs/StartActivity.java
@@ -0,0 +1,13 @@
+package sulijin.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
